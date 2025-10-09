c5cffdc6a5b5ba9bd9396b78130ccad25bfeb486
salahdine <salahdaha7@gmail.com>
2025-10-09 16:11:05 +0100

    Add API documentation setup and middleware enhancements
    - Installed `swagger-jsdoc` and `swagger-ui-express` for API documentation.
    - Added `logger.js` middleware for request logging.
    - Implemented `notFound.js` middleware for 404 error handling.
    - Added Swagger documentation to `productRoutes.js` for product-related APIs.

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 import express from "express";
 import connectDB from "./config/db.js";
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
 import dotenv from "dotenv";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import  logger from "./middlewares/logger.js"
+import  notFound from "./middlewares/notFound.js"
 
 const app = express();
 
@@ -15,13 +19,38 @@ app.use(express.json());
 
 await connectDB(MongoUri);
 
+
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'API Utilisateurs - Exemple Swagger',
+            version: '1.0.0',
+            description: 'Une API simple avec Swagger et Express.js',
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000',
+            },
+        ],
+    },
+    apis: ['./routes/*.js'], // chemin vers les commentaires d’API
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 // app.get('/' , (req , res) => {
 //     res.send("API CONNECTED SUCCESSFULLY");
 // })
 
 app.use('/api/products' , productRoutes);
+
+
 app.use('/api/users' , userRoutes);
 
+app.use(logger);
+app.use(notFound);
 
 app.listen(Port , () => {
     console.log("server successfully connect to http://localhost:3000");
