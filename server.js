18c7df7bd45599a6e45e7d599c8f3f0bf178642e
salahdine <salahdaha7@gmail.com>
2025-10-16 12:09:25 +0100

    feat: Add testing setup

--- server.js
+++ server.js
@@ -12,22 +12,30 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import authRoutes from './routes/auth.js';
 import { authenticate } from './middlewares/authMiddleware.js';
-import cartRoutes from "./routes/cartRouter.js"
+import cartRoutes from "./routes/cartRouter.js";
+
+
 const app = express();
 
 dotenv.config();
 
 const Port = process.env.PORT || 3000;
-const MongoUri = process.env.MONGO_URI;
+const MongoUri = process.env.NODE_ENV === 'test'
+    ? process.env.MONGO_URI_TEST
+    : process.env.MONGO_URI;
 
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(logger);
 
-await connectDB(MongoUri);
+if (process.env.NODE_ENV !== 'test') {
+    await connectDB(MongoUri);
+}
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, { explorer: true }));
+if (process.env.NODE_ENV !== 'test') {
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {explorer: true}));
+}
 
 app.use('/api/auth', authRoutes);
 
@@ -41,7 +49,10 @@ app.use('/api/categories' , categorieRoute);
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(Port , () => {
-    console.log(" server successfully connect to http://localhost:3000");
+export default app;
 
-});
+if (process.env.NODE_ENV !== 'test') {
+    app.listen(Port, () => {
+        console.log(`server successfully connect to http://localhost:${Port}`);
+    });
+}
